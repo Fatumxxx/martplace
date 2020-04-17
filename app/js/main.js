45ad39f897b11dac9024f7e65bcc328a792af803
Fatumxxx <vasylzaya@gmail.com>
2020-04-17 10:44:58 +0300

    added slider products

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function() {
     
-
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "14px",
+        spacing: "3px",
+        ratedFill: "#ffc000",
+        normalFill: "#ffffff"
+    });
     
+    $('.products-slider__inner').slick({
+        arrows: true, 
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prew"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"></button>'
+    });
 });
